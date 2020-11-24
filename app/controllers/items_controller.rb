9818e9d36bd99d977ee600e9991508679ef8064e
Eryk Alexandre Doucet <eadoucet@gmail.com>
2020-11-24 15:04:45 -0500

    created form but have error when clicking submit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,13 +6,24 @@ class ItemsController < ApplicationController
   def destroy
 
   end
-  
-  def new 
 
+  def new
+    @project = Project.find(params[:project_id])
+    @item = Item.new
   end
 
-  def create 
-
+  def create
+    @item = Item.new(item_params)
+    project = params[:project_id]
+    @item.project_id = project
+    if @item.save
+      redirect_to new_project_item_path(project)
+    else
+      render :new
+    end
   end
 
+  def item_params
+    params.require(:item).permit(:name, :comment, :box_id)
+  end
 end
