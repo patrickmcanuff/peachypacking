64edc47ffde3df101eec1625e92620bbb8f404f3
sarah dakkak <sarah.dakkak1@gmail.com>
2020-11-24 22:00:11 -0500

    footer

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,18 +1,2 @@
 class BoxesController < ApplicationController
-  def edit
-
-  end
-
-  def destroy
-
-  end
-
-  def new
-
-  end
-
-  def create
-
-  end
-
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,6 +10,8 @@ class ProjectsController < ApplicationController
   end
 
   def scan
+
+
   end
 
   def index
